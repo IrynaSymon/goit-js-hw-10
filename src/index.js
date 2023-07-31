9aed140fd736e7285c846fffb65d1eab1cafd507
IrynaSymon <126278348+IrynaSymon@users.noreply.github.com>
2023-07-31 15:13:49 +0200

    commit2

--- src/index.js
+++ src/index.js
@@ -9,55 +9,49 @@ const select = document.querySelector(".breed-select");
 const loader = document.querySelector(".loader");
 const error = document.querySelector(".error");
 const catInfo = document.querySelector(".cat-info");
-catInfo.classList.add("is-hidden");
-loader.classList.remove("is-hidden");
-select.classList.add("is-hidden");
-error.classList.add("is-hidden");
-catInfo.classList.add("is-hidden");
+
+ 
     //backend//
-    let arr = [];
+function fetchCatList() {
+    loader.classList.remove("is-hidden");
+    error.classList.add("is-hidden");
+    select.classList.add("is-hidden");
+    catInfo.classList.add("is-hidden");
     fetchBreeds()
         .then(data => {
-            data.forEach(element => {
-                arr.push({text: element.name, value: element.id});
-            });
+            select.innerHTML = data.map(({ id, name }) => `<option value="${id}">${name}</option>`).join('');
             new SlimSelect({
-                select: select,
-                data: arr
-            });
-    loader.classList.add("is-hidden");
-            select.classList.remove("is-hidden");
-
-            
+                select: select
+            })
+            loader.classList.add("is-hidden");
+            select.classList.remove("is-hidden")
         })
         .catch(error => {
             Notify.failure("Oops! Something went wrong! Try reloading the page!")
         });
-
-select.addEventListener("change", onBreedSelect);
-function onBreedSelect(evt) {
-    loader.classList.remove("is-hidden");
-    catInfo.classList.add("is-hidden");
-    const breedId = evt.currentTarget.value;
-    fetchCatByBreed(breedId)
-        .then(data => {
-            wrapperCatInfo(data);
-            loader.classList.add("is-hidden");
-            catInfo.classList.remove("is-hidden");
-            wrapperCatInfo(data);
-    })
-        .catch(error => {
-        loader.classList.add("is-hidden");
-            Notify.failure("Oops! Something went wrong! Try reloading the page!")
-        });
 }
-function wrapperCatInfo(data) {
-     const { url, breeds } = data[0];
-    const deskrCardCat = `<div class="container">
+    fetchCatList();
+    select.addEventListener("change", onBreedSelect);
+    function onBreedSelect(evt) {
+        loader.classList.remove("is-hidden");
+        catInfo.classList.add("is-hidden");
+        const breedId = evt.currentTarget.value;
+        fetchCatByBreed(breedId)
+            .then(data => {
+                loader.classList.add("is-hidden");
+                catInfo.classList.remove("is-hidden");
+                 const { url, breeds } = data[0];
+    catInfo.innerHTML = `<div class="container">
   <img src="${url}" alt="${breeds[0].name}" width ="400px">
 </div><div class="box"><h1>${breeds[0].name}</h1>
 <p>${breeds[0].description}</p>
 <p><span class="temperament">Temperament:</span>${breeds[0].temperament}</p></div>`
-catInfo.innerHTML = deskrCardCat; 
+            })
+            .catch(error => {
+                loader.classList.add("is-hidden");
+                Notify.failure("Oops! Something went wrong! Try reloading the page!")
+            });
+    }
+
     
-}
+   
